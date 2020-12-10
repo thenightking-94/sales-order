931fc38ad2a48bbe5d7a86505eb214ec6e48a622
Shubham Chatterjee <c.suvam@gmail.com>
2020-12-11 02:21:36 +0530

    logger updated design

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -4,28 +4,26 @@ import { Avatar, Typography, Button, Grid } from '@material-ui/core';
 
 const Dashboard = (props) => {
     const [hover_on, sethover] = useState(false);
+
+
+
+
     const logout_option = () => {
         window.location.assign('/logout');
     }
 
-
     return (
         <div>
             <div id='nav'>
-                {window.innerWidth > 768
-                    && <div><Typography className='typo'><i style={{ color: 'black' }}>{props.match.params.name}</i>&nbsp;</Typography>
-                        <p style={{ fontFamily: 'ITC Charter', color: 'black' }}>{localStorage.getItem('email_id')}</p></div>
-                }
-                {window.innerWidth < 768 &&
-                    <div><Typography className='typo'><i style={{ color: 'black' }}>{props.match.params.name}</i>&nbsp;</Typography>
-                        <p style={{ fontFamily: 'Helvetica', fontVariant: 'small-caps', color: 'black' }}>{localStorage.getItem('email_id')}</p></div>
-                }
-                <span id='logged_info'>
-                    {hover_on && <Typography style={{ background: '#e88d14', padding: '3px', borderRadius: '4px', color: 'white' }} className='logged_info'>Log out</Typography>}
+                <div id='logged_info'>
+                    <Typography className='typo'><i style={{ color: 'black' }}>{props.match.params.name}</i></Typography>
+                    &nbsp;&nbsp;
+                    {hover_on && <Typography style={{ background: 'red' }} className='logged_info'>Log out</Typography>}
                     {!hover_on && <Typography className='logged_info'>Logged in</Typography>}
-                    &nbsp;&nbsp;&nbsp;
+                    &nbsp;&nbsp;
                     <Avatar onMouseOver={() => { sethover(true) }} onMouseLeave={() => { sethover(false) }} onClick={logout_option} style={{ cursor: 'pointer' }} src={`${localStorage.getItem('img_url')}`} />
-                </span>
+                </div>
+
             </div>
 
 
