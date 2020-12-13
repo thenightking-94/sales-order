3f2217c707c7a8c22ce444ec3efd1f99c7eaaf4a
Shubham Chatterjee <c.suvam@gmail.com>
2020-12-14 01:32:43 +0530

    code updates

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import XLSX from 'xlsx';
 import { make_cols } from './MakeColumns';
 import { SheetJSFT } from './types';
@@ -12,7 +12,10 @@ import InfoIcon from '@material-ui/icons/Info';
 
 const Dashboard = (props) => {
     const [hover_on, sethover] = useState(false);
-
+    const [uploadComplete, setComplete] = useState(false);
+    const [file, setfile] = useState({});
+    const [data, setdata] = useState([]);
+    const [cols, setcols] = useState([]);
 
     const logout_option = () => {
         window.location.assign('/logout');
@@ -32,14 +35,52 @@ const Dashboard = (props) => {
         }
         return val;
     }
-    const handleChange = () => {
 
+
+
+    useEffect(() => {
+        if (data.length > 0 && cols.length > 0 && uploadComplete)
+            localStorage.setItem('items', JSON.stringify(data));
+        console.log(data)
+    }, [data, cols, uploadComplete])
+
+    const handleChange = (e) => {
+        const files = e.target.files;
+        if (files && files[0])
+            setfile(files[0]);
     }
     const handleFile = () => {
+        /* Boilerplate to set up FileReader */
+        const reader = new FileReader();
+        const rABS = !!reader.readAsBinaryString;
 
+        reader.onload = (e) => {
+            /* Parse data */
+            const bstr = e.target.result;
+            const wb = XLSX.read(bstr, { type: rABS ? 'binary' : 'array', bookVBA: true });
+            /* Get first worksheet */
+            const wsname = wb.SheetNames[0];
+            const ws = wb.Sheets[wsname];
+            /* Convert array of arrays */
+            const data = XLSX.utils.sheet_to_json(ws);
+            /* Update state */
+            setdata(data);
+            setcols(make_cols(ws['!ref']));
+            setComplete(true)
+        };
+
+        if (rABS) {
+            reader.readAsBinaryString(file);
+        } else {
+            reader.readAsArrayBuffer(file);
+        };
     }
+
+
+
     return (
         <div className='background_body'>
+
             <div id='nav'>
                 <input className='searchBox' type='text' placeholder='Search Products....' />
 
@@ -57,21 +98,27 @@ const Dashboard = (props) => {
 
             <div className='sidebar'>
                 <div id='upper_icons'>
-                    <span className="btn_icons"  >
+                    <span style={uploadComplete ? {} : { position: 'fixed', top: '20px' }} className="btn_icons"  >
                         <p className="icon_upper_main" >K</p>
                     </span>
-                    <span className="btn_icons"  >
-                        <ShoppingCartIcon className="icon_upper" />
-                        <p id='side_icon'>Cart</p>
-                    </span>
-                    <span className="btn_icons"  >
-                        <AddShoppingCartIcon className="icon_upper" />
-                        <p id='side_icon'>Add to Cart</p>
-                    </span>
-                    <span className="btn_icons" >
-                        <InfoIcon className="icon_upper" />
-                        <p id='side_icon'>Info</p>
-                    </span>
+                    {
+                        data && uploadComplete && <span className="btn_icons"  >
+                            <ShoppingCartIcon className="icon_upper" />
+                            <p id='side_icon'>Cart</p>
+                        </span>
+                    }
+                    {
+                        data && uploadComplete && <span className="btn_icons"  >
+                            <AddShoppingCartIcon className="icon_upper" />
+                            <p id='side_icon'>Add to Cart</p>
+                        </span>
+                    }
+                    {
+                        data && uploadComplete && <span className="btn_icons" >
+                            <InfoIcon className="icon_upper" />
+                            <p id='side_icon'>Info</p>
+                        </span>
+                    }
                 </div>
             </div>
 
@@ -84,7 +131,7 @@ const Dashboard = (props) => {
 
             <div className='main_body'>
                 <div className='flexAdder'>
-                    <Typography className='typo_body' > Place Order</Typography>
+                    <Typography className='typo_body' ><u> {uploadComplete ? 'Place Order' : 'Upload sales data'}</u></Typography>
                 </div>
 
                 <br />
@@ -92,13 +139,14 @@ const Dashboard = (props) => {
                 <div className='flexAdder'>
                     <div className='salesdata_block'>
                         <p id='para_typo'>
-                            Kindly upload your sales data excel sheet to proceed
+                            Kindly upload your sales data in excel sheet format to proceed
                         </p>
-                        <br/>
-                        <input type="file" className="form-control" id="file" accept={SheetJSFT} onChange={handleChange} />
+                        <br />
+                        <input type="file" className="form-control file_loader" id="file" accept={SheetJSFT} onChange={handleChange} />
                         <br />
                         <input type='submit'
-                            value="Upload Sales data"
+                            className='process_btn'
+                            value="Process your data"
                             onClick={handleFile} />
                     </div>
 
